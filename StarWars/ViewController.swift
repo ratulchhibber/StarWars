f6323fd96caa21c08f50016f81ef828f9ae0f5e9
Ratul Chhibber <ratulchhibber@gmail.com>
2020-09-15 23:56:55 +0530

    Initial project

---
+++ StarWars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWars
+//
+//  Created by Ratul Chhibber on 15/09/20.
+//  Copyright © 2020 Ratul Chhibber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
