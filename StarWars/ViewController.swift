66b9239dcd3b5ae71752b749e9e65f91f57d5e2b
Ratul Chhibber <ratulchhibber@gmail.com>
2020-09-16 00:56:27 +0530

    * Pod init * Adding libraries - Moya, Lottie

--- StarWars/ViewController.swift
+++ StarWars/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // Create Animation object
+        view.backgroundColor = .black
+        let jsonName = "starWars"
+        let animation = Animation.named(jsonName)
+
+        // Load animation to AnimationView
+        let animationView = AnimationView(animation: animation)
+        animationView.frame = view.frame
+        animationView.loopMode = .repeat(2)
+
+        // Add animationView as subview
+        view.addSubview(animationView)
+
+        // Play the animation
+        animationView.play { isFinished in
+            print("Completed")
+        }
     }
 
 
