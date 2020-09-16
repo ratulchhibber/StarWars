83c0253b0c80f98fe12649f1a3839c52c6dd0700
Ratul Chhibber <ratulchhibber@gmail.com>
2020-09-17 01:38:43 +0530

    Home Page - Showcasing Characters

---
+++ StarWars/View/CharactersVC.swift
@@ -0,0 +1,81 @@
+//
+//  CharactersVC.swift
+//  StarWars
+//
+//  Created by Ratul Chhibber on 15/09/20.
+//  Copyright © 2020 Ratul Chhibber. All rights reserved.
+//
+
+import UIKit
+import Lottie
+import RxSwift
+
+class CharactersVC: UIViewController {
+    
+    @IBOutlet private var charactersTableView: UITableView!
+    private let disposeBag = DisposeBag()
+    
+    private let viewModel = CharactersVM()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Star Wars: Characters".localized()
+        triggerFetchCharacters()
+    }
+    
+    private func triggerFetchCharacters() {
+        ActivityIndicator.shared.show(in: view)
+        viewModel.fetchCharacters()
+            .subscribe(onCompleted: {
+                ActivityIndicator.shared.hide()
+                self.charactersTableView.reloadData()
+            }, onError: { error in
+                ActivityIndicator.shared.hide()
+                // Show alert
+                print(error.localizedDescription)
+            }).disposed(by: disposeBag)
+    }
+    
+    private func triggerFetchFilms(for index: Int) {
+        ActivityIndicator.shared.show(in: view)
+        viewModel.fetchFilms(for: index)
+            .subscribe(onCompleted: {
+                ActivityIndicator.shared.hide()
+                self.routeToCharacterDetails(for: index)
+                print(self.viewModel.dataModel(for: index)
+                )
+            }) { (error) in
+                ActivityIndicator.shared.hide()
+
+                // show alert
+        }.disposed(by: disposeBag)
+    }
+    
+    private func routeToCharacterDetails(for index: Int) {
+        let model = viewModel.dataModel(for: index)
+        guard let view = ViewFactory.createDetailView(for: model) else {
+            return
+        }
+        navigationController?.pushViewController(view, animated: true)
+    }
+}
+
+extension CharactersVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "characterTitle",
+                                                 for: indexPath)
+        cell.textLabel?.text = viewModel.characters[indexPath.row]
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.characters.count
+    }
+}
+
+extension CharactersVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        triggerFetchFilms(for: indexPath.row)
+    }
+}
---
+++ StarWars/ViewModel/CharactersVM.swift
@@ -0,0 +1,87 @@
+//
+//  CharactersVM.swift
+//  StarWars
+//
+//  Created by Ratul Chhibber on 16/09/20.
+//  Copyright © 2020 Ratul Chhibber. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import Moya
+
+class CharactersVM {
+    var characterModel: Characters?
+    var filmModel = [Film]()
+    let provider = MoyaProvider<API>()
+    let network = NetworkService()
+    
+    var characters: [String] {
+        var characters = [String]()
+        characterModel?.results.forEach {
+            characters.append($0.name)
+        }
+        return characters
+    }
+    
+    private func getFilmIds(for selectedIndex: Int) -> [String] {
+        filmModel.removeAll()
+        let characterName = characters[selectedIndex]
+        guard let movies = characterModel?.results.first(where: { result -> Bool in
+            result.name == characterName
+        }) else {
+            return []
+        }
+        
+        return movies.films.map({
+            ($0.dropLast().components(separatedBy: "/").last ?? "")
+        })
+    }
+}
+
+//MARK: Routing Data Model
+struct CharacterDataModel {
+    let character: Result?
+    let films: [Film]
+}
+
+extension CharactersVM {
+    
+    func dataModel(for index: Int) -> CharacterDataModel {
+        return CharacterDataModel(character: characterModel?.results[index],
+                                  films: filmModel)
+    }
+}
+
+//MARK: Network Requests
+extension CharactersVM {
+    func fetchCharacters() -> Completable {
+        return .create { observer in
+            self.network.fetchCharacters()
+                .subscribe(onSuccess: {
+                    self.characterModel = $0
+                    observer(.completed)
+                }, onError: { error in
+                    observer(.error(error))
+                })
+        }
+    }
+    
+    func fetchFilms(for selectedId: Int) -> Completable {
+        let allCompletables = getFilmIds(for: selectedId)
+                              .map { fetchFilm(for: $0) }
+        return Completable.zip(allCompletables)
+    }
+    
+    private func fetchFilm(for filmId: String)  -> Completable {
+        return .create { observer in
+            self.network.fetchFilm(for: filmId)
+                .subscribe(onSuccess: {
+                    self.filmModel.append($0)
+                    observer(.completed)
+                }, onError: { error in
+                    observer(.error(error))
+                })
+        }
+    }
+}
